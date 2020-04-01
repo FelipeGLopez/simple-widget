4c64aeef8263ca207be40d41229edb01c4b37bf5
Felipe <fglf18@gmail.com>
2020-03-31 23:37:59 -0300

    First console output of new messages with api

--- TelegramWidget/app/src/main/java/com/example/telegramwidget/MainActivity.java
+++ TelegramWidget/app/src/main/java/com/example/telegramwidget/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.telegramwidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import org.drinkless.td.libcore.telegram.TdApi;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
